b57679b2e977d7770fe1c517e980cd24b5ab5580
Daigan Berger <daiganberger@gmail.com>
2025-09-04 09:21:35 -0500

    done

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const button = document.querySelector(".cool-button")
 button.addEventListener("click", () => {
 	button.classList.toggle("goofy")
 });
+
+
+document.querySelector(".name-submit").addEventListener("click", () => {
+	const name = (document.querySelector(".name-input")).value;
+	console.log(name)
+	const newLI = document.createElement("li");
+	newLI.innerHTML = name;
+	document.querySelector(".list").appendChild(newLI)
+});
